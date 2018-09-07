53502455aa9541302c4dfb03157a39a64da79388
Scott Weaver <scottmweaver@gmail.com>
2018-09-07 13:41:04 -0700

    #1: Add launchlibrary.net URL to replace spacexdata

--- spacebar.30s.js
+++ spacebar.30s.js
@@ -5,61 +5,49 @@
 //# <bitbar.version>v1.0</bitbar.version>
 //# <bitbar.author>Scott Weaver</bitbar.author>
 //# <bitbar.author.github>tdlm</bitbar.author.github>
-//# <bitbar.desc>Displays upcoming SpaceX Launches.</bitbar.desc>
+//# <bitbar.desc>Displays upcoming Space Launches.</bitbar.desc>
 
 const bitbar = require('bitbar');
 const { httpGet } = require('./lib/helpers');
 const moment = require('moment');
 
 // Get upcoming launches
-httpGet('https://api.spacexdata.com/v2/launches/upcoming')
+httpGet('https://launchlibrary.net/1.4/launch')
     .then(response => {
         let output = [];
 
         output.push({
-            text: 'SpaceX Launches',
+            text: 'Space Launches',
             color: '#333',
             dropdown: false
         });
 
         output.push(bitbar.sep);
 
-        response.forEach(launch => {
+        response.launches.forEach(launch => {
             let submenu = [];
 
             submenu.push({
-                text: 'Rocket: ' + launch.rocket.rocket_name,
+                text: 'Rocket: ' + launch.name,
                 color: 'black'
             });
 
             submenu.push({
-                text: 'Launching from: ' + launch.launch_site.site_name_long,
+                text: 'Time to launch: ' + moment.utc(launch.windowstart).fromNow(),
                 color: 'black'
             });
 
-            submenu.push({
-                text:
-                    'Time to launch: ' +
-                    moment.utc(launch.launch_date_utc).fromNow(),
-                color: 'black'
-            });
-
-            if (null !== launch.details) {
-                submenu.push({
-                    text: 'Detail: ' + launch.details.trunc(75),
-                    color: 'black'
-                });
-            }
-
-            if (null !== launch.links.video_link) {
-                submenu.push({
-                    text: 'Video',
-                    href: launch.links.video_link
+            if ('object' === typeof launch.vidURLs) {
+                launch.vidURLs.forEach(video_url => {
+                    submenu.push({
+                        text: 'Video: ' + video_url,
+                        href: video_url
+                    });
                 });
             }
 
             output.push({
-                text: 'Launch #' + launch.flight_number,
+                text: 'Launch #X',
                 color: 'black',
                 submenu: submenu
             });
